e818a85350113e32ed9bb58b2a4af87a58f99e94
Wingedwhisper14 <15645434+Wingedwhisper14@users.noreply.github.com>
2026-02-12 02:53:25 +0530

    Fixed prod routing

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
